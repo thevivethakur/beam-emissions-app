386d40044211351faf5349fae58222cb683f8056
thevivethakur <121496427+thevivethakur@users.noreply.github.com>
2025-07-02 16:39:23 +0530

    Add files via upload

---
+++ entities.js
@@ -0,0 +1,13 @@
+import { base44 } from './base44Client';
+
+
+export const Project = base44.entities.Project;
+
+export const Assessment = base44.entities.Assessment;
+
+export const Material = base44.entities.Material;
+
+
+
+// auth sdk:
+export const User = base44.auth;
---
+++ functions.js
@@ -0,0 +1,9 @@
+import { base44 } from './base44Client';
+
+
+export const processPDFFile = base44.functions.processPDFFile;
+
+export const processExcelFile = base44.functions.processExcelFile;
+
+export const processExcelMaterials = base44.functions.processExcelMaterials;
+
